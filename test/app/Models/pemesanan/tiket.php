e48dc4c0dcdef036e19fea997cb3654580e81217
akusenganyar <74917704+akusenganyar@users.noreply.github.com>
2022-07-08 21:07:31 +0700

    Add files via upload

---
+++ test/app/Models/pemesanan/tiket.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\pemesanan;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tiket extends Model
+{
+    use HasFactory;
+}
