b093a7062d938393fe4b546ab73bd02b9280d4b4
akusenganyar <74917704+akusenganyar@users.noreply.github.com>
2022-07-08 21:14:54 +0700

    Add files via upload

---
+++ test/database/migrations/2022_06_30_183912_create_tikets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tikets', function (Blueprint $table) {
+            $table->id('id_tiket');
+            $table->bigInteger('penerbangan_id');
+            $table->date('tanggal_pesan');
+            $table->date('tanggal_berangkat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tikets');
+    }
+};
